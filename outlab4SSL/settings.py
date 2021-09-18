22eaeaea496325ded6c4424a55b40700a37b278b
Gurpreet Singh <gurpreet@cse.iitb.ac.in>
2021-09-18 20:07:12 +0530

    updated database

--- outlab4SSL/settings.py
+++ outlab4SSL/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'outlab4SSL.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('DB_NAME'),
         'USER':config('DB_USER'),
         'PASSWORD':config('DB_PASSWORD'),
         'HOST':config('DB_HOST'),  
-
+        'PORT':config('DB_PORT'),
     }
 }
 
